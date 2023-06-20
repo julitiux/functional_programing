68dfb154cc1595ca5bc0dda716b4a9b64a71435a
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-19 18:46:16 -0600

    add project java using gradle

---
+++ src/test/java/com/functionalPrograming/FunctionalProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.functionalPrograming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgramingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
