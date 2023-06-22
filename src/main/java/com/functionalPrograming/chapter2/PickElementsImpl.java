4077600d521f4ea41a8a7e5bdc2b2c35615d63e9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 16:40:20 -0600

    add my first predicate

--- src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
+++ src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
@@ -1,14 +1,18 @@
 package com.functionalPrograming.chapter2;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class PickElementsImpl implements PickElements {
 
+  // THIS WAS MY FIRST PREDICATE
+  final Predicate<String> firstPredicate = word -> word.startsWith("N");
+
   @Override
-  public List<String> startsWithXLetter(List<String> wordList, String startWtih) {
+  public List<String> startsWithXLetter(List<String> wordList, String startWith) {
     return wordList.stream()
-      .filter(word -> word.startsWith(startWtih))
+      .filter(firstPredicate)
       .collect(Collectors.toList());
   }
 
