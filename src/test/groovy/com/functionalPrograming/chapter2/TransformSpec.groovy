5e2fbce1e0678d2e9df485ce0c61226521ba3ff3
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 13:24:00 -0600

    add implementations test about service.transformUpperCase

--- src/test/groovy/com/functionalPrograming/chapter2/TransformSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter2/TransformSpec.groovy
@@ -5,7 +5,17 @@ import spock.lang.Specification
 class TransformSpec extends Specification {
 
   def ""() {
-
+    given:
+    Transform service = new TransformImpl()
+    and:
+    List<String> nameList = ['Juanita', 'Calambrita',' Pedrito']
+    and:
+    List<String> response
+    when:
+    response = service.transformUpperCase(nameList)
+    then:
+    println response
+    response
   }
-  
+
 }
