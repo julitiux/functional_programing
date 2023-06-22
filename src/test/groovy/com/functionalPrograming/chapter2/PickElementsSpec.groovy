993a86ccbd1d33fffcc1f7b57ee53f67743bbee7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 16:14:07 -0600

    implement Spec PickElements

--- src/test/groovy/com/functionalPrograming/chapter2/PickElementsSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter2/PickElementsSpec.groovy
@@ -1,4 +1,23 @@
 package com.functionalPrograming.chapter2
 
-class PickElementsSpec {
+import spock.lang.Specification
+
+class PickElementsSpec extends Specification {
+
+  def ""() {
+    given:
+    PickElements service = new PickElementsImpl()
+    and:
+    List<String> wordList = _wordList
+    and:
+    List<String> response
+    when:
+    response = service.startsWithXLetter(wordList)
+    then:
+    response == _response
+    where:
+    _wordList                                          | _response
+    ["Brian", "Nate", "Neal", "Raju", "Sara", "Scott"] | ["Nate", "Neal"]
+  }
+
 }
