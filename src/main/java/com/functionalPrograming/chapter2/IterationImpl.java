abd99f76fe23d707c0613f7bc02b903495a8db82
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-21 18:47:54 -0600

    the forEach is higher-order function thats accepts a lambda experssion or block of code to execute in the context of each element in the list

--- src/main/java/com/functionalPrograming/chapter2/IterationImpl.java
+++ src/main/java/com/functionalPrograming/chapter2/IterationImpl.java
@@ -3,20 +3,12 @@ package com.functionalPrograming.chapter2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.function.Consumer;
 
 @Service
 class IterationImpl implements Iteration {
 
   @Override
   public void iteration(List<String> stringList) {
-    stringList.forEach(new Consumer<String>() {
-      @Override
-      public void accept(String name) {
-        System.out.println(name);
-      }
-    });
-
-
+    stringList.forEach((final String name) -> System.out.println(name));
   }
 }
