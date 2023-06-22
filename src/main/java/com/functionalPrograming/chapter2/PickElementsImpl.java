f34153bf86f84c18f80136bf9f2d9da2fadbdab2
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 16:40:47 -0600

    this is my secod predicate with a param implemented

--- src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
+++ src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
@@ -9,10 +9,14 @@ public class PickElementsImpl implements PickElements {
   // THIS WAS MY FIRST PREDICATE
   final Predicate<String> firstPredicate = word -> word.startsWith("N");
 
+  final Predicate<String> startWithPredicate(String start) {
+    return it -> it.startsWith(start);
+  }
+
   @Override
   public List<String> startsWithXLetter(List<String> wordList, String startWith) {
     return wordList.stream()
-      .filter(firstPredicate)
+      .filter(startWithPredicate(startWith))
       .collect(Collectors.toList());
   }
 
