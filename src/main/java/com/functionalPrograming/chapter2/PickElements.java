223d4ac51c99249933586c58241cb888eb71fcdb
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 16:19:28 -0600

    add a second parameter startWith

--- src/main/java/com/functionalPrograming/chapter2/PickElements.java
+++ src/main/java/com/functionalPrograming/chapter2/PickElements.java
@@ -3,5 +3,5 @@ package com.functionalPrograming.chapter2;
 import java.util.List;
 
 public interface PickElements {
-  List<String> startsWithXLetter(List<String> wordList);
+  List<String> startsWithXLetter(List<String> wordList, String startWtih);
 }
