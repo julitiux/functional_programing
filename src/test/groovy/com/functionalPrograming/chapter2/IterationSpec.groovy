66a1779a691175c5e8cc4e3683d8be2c7ac82a70
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-21 18:50:59 -0600

    implementa unit test IterationSpec

---
+++ src/test/groovy/com/functionalPrograming/chapter2/IterationSpec.groovy
@@ -0,0 +1,16 @@
+package com.functionalPrograming.chapter2
+
+import spock.lang.Specification
+
+class IterationSpec extends Specification {
+
+
+  def ""() {
+    given:
+    Iteration service = new IterationImpl()
+    and:
+    List nameList = ["Brian", "Nate", "Neal", "Raju", "Sara", "Scott"]
+    expect:
+    service.iteration(nameList)
+  }
+}
