caf4b2273d40fa6d4c4954d4d020774a5ed67699
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 18:46:30 -0600

    implement a Function with param in String an out a Predicate

--- src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
+++ src/main/java/com/functionalPrograming/chapter2/PickElementsImpl.java
@@ -1,6 +1,7 @@
 package com.functionalPrograming.chapter2;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,12 @@ public class PickElementsImpl implements PickElements {
   final Predicate<String> startWithPredicate(final String letter) {
     return it -> it.startsWith(letter);
   }
+
+  final Function<String, Predicate<String>> startWithLetter =
+    (String letter) -> {
+      Predicate<String> checkStarts = (String word) -> word.startsWith(letter);
+      return checkStarts;
+    };
 
   @Override
   public List<String> startsWithXLetter(List<String> wordList, String startWith) {
