0142a300e4d0660dd5bf66b71b299ad5cc522e8e
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-22 20:08:30 -0600

    implement method pickALongName

--- src/main/java/com/functionalPrograming/chapter2/PickALongestImpl.java
+++ src/main/java/com/functionalPrograming/chapter2/PickALongestImpl.java
@@ -3,6 +3,7 @@ package com.functionalPrograming.chapter2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PickALongestImpl implements PickALongest {
@@ -11,5 +12,12 @@ public class PickALongestImpl implements PickALongest {
     Integer dummy = nameList.stream().mapToInt(name -> name.length()).sum();
     System.out.println("Total member of characters in all names: " + dummy);
     return dummy;
+  }
+
+  @Override
+  public String pickALongName(List<String> nameList) {
+    Optional<String> aLongName = nameList.stream().reduce((name1, name2) -> name1.length() >= name2.length() ? name1 : name2);
+    aLongName.ifPresent(name -> System.out.println("A longest name: " + name));
+    return aLongName.orElse("NO ONE");
   }
 }
