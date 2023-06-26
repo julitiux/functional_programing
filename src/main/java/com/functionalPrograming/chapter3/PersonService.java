22ce547d4d869ce651ccbb16c26d879605064f0f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 02:32:47 -0500

    declare a empty interface and implementation class PersonService

---
+++ src/main/java/com/functionalPrograming/chapter3/PersonService.java
@@ -0,0 +1,7 @@
+package com.functionalPrograming.chapter3;
+
+import java.util.List;
+
+public interface PersonService {
+  List<Person> ascendingAge(List<Person> personList);
+}
---
+++ src/main/java/com/functionalPrograming/chapter3/PersonServiceImpl.java
@@ -0,0 +1,12 @@
+package com.functionalPrograming.chapter3;
+
+import java.util.List;
+
+public class PersonServiceImpl implements PersonService {
+
+  @Override
+  public List<Person> ascendingAge(List<Person> personList) {
+    return null;
+  }
+
+}
