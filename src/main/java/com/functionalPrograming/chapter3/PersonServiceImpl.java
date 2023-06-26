c8cc0cbdbe3c0f70a02b712728879082ab80eb5d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 02:48:16 -0500

    implementa a sort using a method person1.ageDifference and collect(Collectors.toList())

--- src/main/java/com/functionalPrograming/chapter3/PersonServiceImpl.java
+++ src/main/java/com/functionalPrograming/chapter3/PersonServiceImpl.java
@@ -1,12 +1,15 @@
 package com.functionalPrograming.chapter3;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PersonServiceImpl implements PersonService {
 
   @Override
   public List<Person> ascendingAge(List<Person> personList) {
-    return null;
+    return personList.stream()
+      .sorted((person1, person2) -> person1.ageDifference(person2))
+      .collect(Collectors.toList());
   }
 
 }
