879911fab68c433a1464bf344584403a44e76bcd
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 02:28:01 -0500

    implement Person class

---
+++ src/main/java/com/functionalPrograming/chapter3/Person.java
@@ -0,0 +1,32 @@
+package com.functionalPrograming.chapter3;
+
+public class Person {
+  String name;
+  int age;
+
+  public Person(final String theName, final int theAge) {
+    this.name = theName;
+    this.age = theAge;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+      "name='" + name + '\'' +
+      ", age=" + age +
+      '}';
+  }
+}
