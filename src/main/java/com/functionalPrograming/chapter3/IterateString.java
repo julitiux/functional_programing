dfb06aecaf7b2a8319b4dbde8fdd3f12b7b442a4
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 01:56:44 -0500

    add interface and implementation about IterateString class

---
+++ src/main/java/com/functionalPrograming/chapter3/IterateString.java
@@ -0,0 +1,5 @@
+package com.functionalPrograming.chapter3;
+
+public interface IterateString {
+  void iterateString(String string);
+}
---
+++ src/main/java/com/functionalPrograming/chapter3/IterateStringImpl.java
@@ -0,0 +1,10 @@
+package com.functionalPrograming.chapter3;
+
+public class IterateStringImpl implements IterateString {
+
+  @Override
+  public void iterateString(String string) {
+
+  }
+
+}
