c7f7abf24ed01c6fd651a07df33a424a9cd76546
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 16:44:34 -0500

    declare method peopleByAge and return a Map with Integer and a list of People

--- src/main/java/com/functionalPrograming/chapter3/OlderThan20.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20.java
@@ -1,7 +1,9 @@
 package com.functionalPrograming.chapter3;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OlderThan20 {
   List<Person> olderThan20(List<Person> personList);
+  Map<Integer, List<Person>> peopleByAge(List<Person> personList);
 }
