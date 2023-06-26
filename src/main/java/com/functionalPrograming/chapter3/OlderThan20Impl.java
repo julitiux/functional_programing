7b1f6d0115da03e028a13a17e06a07ba55a1a18b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 16:34:14 -0500

    the method collect can perform parallel additions, as appropiate, into different sublist, and then merge them in a thread-dafe manner into a lager list (hence the last parameter to help merge lists)

--- src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
@@ -7,11 +7,9 @@ public class OlderThan20Impl implements OlderThan20 {
 
   @Override
   public List<Person> olderThan20(List<Person> personList) {
-    List<Person> olderThan20 = new ArrayList<>();
-    personList.stream()
+    return personList.stream()
       .filter(person -> person.getAge() > 20)
-      .forEach(person -> olderThan20.add(person));
-    return olderThan20;
+      .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
   }
 
 }
