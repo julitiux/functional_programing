75623d88eace26c077cde3be8c140efde34f89c9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 15:05:55 -0500

    implement unit test sort descending age

--- src/test/groovy/com/functionalPrograming/chapter3/PersonServiceSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter3/PersonServiceSpec.groovy
@@ -14,4 +14,15 @@ class PersonServiceSpec extends Specification {
     then:
     response == personList.sort { person1, person2 -> person1.age <=> person2.age }
   }
+
+  def "sort descending age"() {
+    given:
+    PersonService service = new PersonServiceImpl()
+    List<Person> personList = Arrays.asList(new Person("Greg", 35), new Person("Jhon", 20), new Person("Sara", 21), new Person("Jane", 21))
+    List<Person> response
+    when:
+    response = service.descendingAge(personList)
+    then:
+    response == personList.sort { person1, person2 -> person2.age <=> person1.age }
+  }
 }
