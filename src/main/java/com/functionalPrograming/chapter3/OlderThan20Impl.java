097158d5ab0473d0ca4079ec94ecc00daaf60f96
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 16:20:10 -0500

    implement method olderThan20

--- src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
@@ -1,4 +1,17 @@
 package com.functionalPrograming.chapter3;
 
-public class OlderThan20Impl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OlderThan20Impl implements OlderThan20 {
+
+  @Override
+  public List<Person> olderThan20(List<Person> personList) {
+    List<Person> olderThan20 = new ArrayList<>();
+    personList.stream()
+      .filter(person -> person.getAge() > 20)
+      .forEach(person -> olderThan20.add(person));
+    return olderThan20;
+  }
+
 }
