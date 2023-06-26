bb471de24b29b80e144da2a17cf2bfbb91ea45cd
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-26 16:44:50 -0500

    implement method peopleByAge

--- src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
@@ -1,6 +1,7 @@
 package com.functionalPrograming.chapter3;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OlderThan20Impl implements OlderThan20 {
@@ -10,6 +11,12 @@ public class OlderThan20Impl implements OlderThan20 {
     return personList.stream()
       .filter(person -> person.getAge() > 20)
       .collect(Collectors.toList());
+  }
+
+  @Override
+  public Map<Integer, List<Person>> peopleByAge(List<Person> personList) {
+    return personList.stream()
+      .collect(Collectors.groupingBy(Person::getAge));
   }
 
 }
