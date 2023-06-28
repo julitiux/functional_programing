6e8b56ccf42a2624c37d8c623dc51509808b0a68
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-27 19:33:48 -0500

    declare the method odestPersonOfEachLetter and return Map<Character, Optiona<Person>>

--- src/main/java/com/functionalPrograming/chapter3/OlderThan20.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20.java
@@ -2,9 +2,11 @@ package com.functionalPrograming.chapter3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface OlderThan20 {
   List<Person> olderThan20(List<Person> personList);
   Map<Integer, List<Person>> peopleByAge(List<Person> personList);
   Map<Integer, List<String>> nameOfPeopleByAge(List<Person> personList);
+  Map<Character, Optional<Person>> odestPersonOfEachLetter(List<Person> personList);
 }
--- src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
+++ src/main/java/com/functionalPrograming/chapter3/OlderThan20Impl.java
@@ -1,11 +1,13 @@
 package com.functionalPrograming.chapter3;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 public class OlderThan20Impl implements OlderThan20 {
 
@@ -26,6 +28,12 @@ public class OlderThan20Impl implements OlderThan20 {
   public Map<Integer, List<String>> nameOfPeopleByAge(List<Person> personList) {
     return personList.stream()
       .collect(Collectors.groupingBy(Person::getAge, mapping(Person::getName, toList())));
+  }
+
+  @Override
+  public Map<Character, Optional<Person>> odestPersonOfEachLetter(List<Person> personList) {
+    return personList.stream()
+      .collect(groupingBy(person -> person.getName().charAt(0), reducing(BinaryOperator.maxBy(byAge))));
   }
 
 }
