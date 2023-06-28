91470c06372095785347efd2c632b12981f8c3da
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-27 20:07:01 -0500

    modify the method declare newDirectoryStream and now return DirectoryStream<Path>

--- src/main/java/com/functionalPrograming/chapter3/ListSelectFiles.java
+++ src/main/java/com/functionalPrograming/chapter3/ListSelectFiles.java
@@ -1,6 +1,12 @@
 package com.functionalPrograming.chapter3;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+
 public interface ListSelectFiles {
 
   String[] getfiles();
+  DirectoryStream<Path> newDirectoryStream() throws IOException;
+
 }
