4b282fc4ad461f1e7e70072f3340517f22a95df0
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 14:27:39 -0500

    implement method totalBondValues

--- src/main/java/com/functionalPrograming/chapter4/AssetUtilImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/AssetUtilImpl.java
@@ -2,11 +2,16 @@ package com.functionalPrograming.chapter4;
 
 import java.util.List;
 
-public class AssetUtilImpl implements AssetUtil{
+public class AssetUtilImpl implements AssetUtil {
 
   @Override
   public Integer totalAssetValues(List<Asset> assetList) {
     return assetList.stream().mapToInt(asset -> asset.getValue()).sum();
+  }
+
+  @Override
+  public Integer totalBondValues(List<Asset> assetList) {
+    return assetList.stream().mapToInt(asset -> asset.getType() == Asset.AssetType.BOND ? asset.getValue() : 0).sum();
   }
 
 }
