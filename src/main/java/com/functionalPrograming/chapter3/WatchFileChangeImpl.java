ceb0173983d7f118e7dd5d139c60bcae00479788
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 00:26:15 -0500

    implement the method watchFileChange

--- src/main/java/com/functionalPrograming/chapter3/WatchFileChangeImpl.java
+++ src/main/java/com/functionalPrograming/chapter3/WatchFileChangeImpl.java
@@ -1,4 +1,19 @@
 package com.functionalPrograming.chapter3;
 
-public class WatchFileChangeImpl {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchService;
+
+public class WatchFileChangeImpl implements WatchFileChange{
+
+  @Override
+  public void watchFileChange() throws IOException {
+    Path path = Paths.get(".");
+    WatchService watchService = path.getFileSystem().newWatchService();
+
+    path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+  }
+
 }
