89d7cb4e004a1dee2ff3d11ec3a78cab0946d6b1
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 18:44:16 -0500

    using clausure where, adding _response and _lambda like parameters

--- src/test/groovy/com/functionalPrograming/chapter4/AssetUtilSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter4/AssetUtilSpec.groovy
@@ -2,17 +2,23 @@ package com.functionalPrograming.chapter4
 
 import spock.lang.Specification
 
+import java.util.function.Predicate
+
 class AssetUtilSpec extends Specification {
 
   def "total asset values"() {
     given:
     AssetUtil service = new AssetUtilImpl();
     List<Asset> assetList = [new Asset(Asset.AssetType.BOND, 1000), new Asset(Asset.AssetType.BOND, 2000), new Asset(Asset.AssetType.STOCK, 3000), new Asset(Asset.AssetType.STOCK, 4000)]
+    Predicate<Asset> assetPredicate = _lambda
     Integer response
     when:
-    response = service.totalAssetValues(assetList, asset -> true)
+    response = service.totalAssetValues(assetList, assetPredicate)
     then:
-    response == 10000
+    response == _response
+    where:
+    _response | _lambda
+    10000     | (asset) -> true
   }
 
   def "total bond values"() {
