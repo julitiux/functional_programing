2adeb4b12bc803447e91bdc8a83c3591ab40941e
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 14:41:26 -0500

    refactor, add a parameter Predicate assetPredicate

--- src/main/java/com/functionalPrograming/chapter4/AssetUtil.java
+++ src/main/java/com/functionalPrograming/chapter4/AssetUtil.java
@@ -1,9 +1,10 @@
 package com.functionalPrograming.chapter4;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public interface AssetUtil {
-  Integer totalAssetValues(List<Asset> assetList);
+  Integer totalAssetValues(List<Asset> assetList, Predicate<Asset> assetPredicate);
   Integer totalBondValues(List<Asset> assetList);
   Integer totalStockValues(List<Asset> assetList);
 }
