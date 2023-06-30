198d06e50cb9201afd1acdee01fe6fcbbec9cee7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 00:26:33 -0500

    implement the spec watch file change

--- src/test/groovy/com/functionalPrograming/chapter3/WatchFileChangeSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter3/WatchFileChangeSpec.groovy
@@ -3,4 +3,11 @@ package com.functionalPrograming.chapter3
 import spock.lang.Specification
 
 class WatchFileChangeSpec extends Specification {
+
+  def "watch file change"() {
+    setup:
+    WatchFileChange service = new WatchFileChangeImpl()
+    expect:
+    service.watchFileChange()
+  }
 }
