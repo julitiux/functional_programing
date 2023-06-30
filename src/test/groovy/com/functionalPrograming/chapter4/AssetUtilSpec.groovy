3ac1fec4a980c56da3a36bc42fcedba63f64ed13
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 02:04:56 -0500

    implement spec total asset values

--- src/test/groovy/com/functionalPrograming/chapter4/AssetUtilSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter4/AssetUtilSpec.groovy
@@ -3,4 +3,16 @@ package com.functionalPrograming.chapter4
 import spock.lang.Specification
 
 class AssetUtilSpec extends Specification {
+
+  def "total asset values"() {
+    given:
+    AssetUtil service = new AssetUtilImpl();
+    List<Asset> assetList = [new Asset(Asset.AssetType.BOND, 1000), new Asset(Asset.AssetType.BOND, 2000), new Asset(Asset.AssetType.STOCK, 3000), new Asset(Asset.AssetType.STOCK, 4000)]
+    Integer response
+    when:
+    response = service.totalAssetValues(assetList)
+    then:
+    response == 10000
+  }
+
 }
