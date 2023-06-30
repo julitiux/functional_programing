c0ab4fd0bc0244f0034bf8dddefdaad7f191e230
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 14:42:02 -0500

    refactor: add a filter and use a reference method on mapToInt

--- src/main/java/com/functionalPrograming/chapter4/AssetUtilImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/AssetUtilImpl.java
@@ -1,12 +1,13 @@
 package com.functionalPrograming.chapter4;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AssetUtilImpl implements AssetUtil {
 
   @Override
-  public Integer totalAssetValues(List<Asset> assetList) {
-    return assetList.stream().mapToInt(asset -> asset.getValue()).sum();
+  public Integer totalAssetValues(List<Asset> assetList, Predicate<Asset> assetPredicate) {
+    return assetList.stream().filter(assetPredicate).mapToInt(Asset::getValue).sum();
   }
 
   @Override
