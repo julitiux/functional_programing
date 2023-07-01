805dd2648ac683e79441359e3b7aa18bf3ea200c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 22:02:59 -0500

    reformat unit test scompute stokc worth

--- src/test/groovy/com/functionalPrograming/chapter4/CalculateNAVSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter4/CalculateNAVSpec.groovy
@@ -8,14 +8,16 @@ class CalculateNAVSpec extends Specification {
 
   def "compute stock worth"() {
     given:
-    CalculateNAV service = new CalculateNAVImpl((ticker) -> new BigDecimal("6.01"))
-    String ticket = "GOOasdasdG"
+    CalculateNAV service = new CalculateNAVImpl(_function)
+    String ticket = "GOOG"
     int shares = 1000
     BigDecimal response
     when:
     response = service.computeStockWorth(ticket, shares)
     then:
-    println response
-
+    response == _response
+    where:
+    _response | _function
+    6010.00   | (ticker) -> new BigDecimal("6.01")
   }
 }
