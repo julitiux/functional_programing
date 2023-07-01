8fb6167cf2b5266c5df3c137317325dbc9ac98e6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 21:45:00 -0500

    implement contructor and this recive a Function with params String and return a BigDecimal

--- src/main/java/com/functionalPrograming/chapter4/CalculateNAVImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/CalculateNAVImpl.java
@@ -1,8 +1,15 @@
 package com.functionalPrograming.chapter4;
 
 import java.math.BigDecimal;
+import java.util.function.Function;
 
 public class CalculateNAVImpl implements CalculateNAV {
+
+  public CalculateNAVImpl(Function<String, BigDecimal> princeFinder) {
+    this.princeFinder = princeFinder;
+  }
+
+  Function<String,BigDecimal> princeFinder;
 
   @Override
   public BigDecimal computeStockWorth(String ticket, int shares) {
