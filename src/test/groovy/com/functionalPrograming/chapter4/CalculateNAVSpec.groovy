51dc1600598871fdcb609f5df97fddc4157f306d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 21:45:52 -0500

    implement unit test and in the new instance if CalculateNAVImpl, send a Function into a contructor

--- src/test/groovy/com/functionalPrograming/chapter4/CalculateNAVSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter4/CalculateNAVSpec.groovy
@@ -1,6 +1,21 @@
 package com.functionalPrograming.chapter4
 
+
 import spock.lang.Specification
 
-class CalculateNAVSpec extends Specification{
+class CalculateNAVSpec extends Specification {
+
+
+  def "compute stock worth"() {
+    given:
+    CalculateNAV service = new CalculateNAVImpl((ticker) -> new BigDecimal("6.01"))
+    String ticket = "GOOasdasdG"
+    int shares = 1000
+    BigDecimal response
+    when:
+    response = service.computeStockWorth(ticket, shares)
+    then:
+    println response
+
+  }
 }
