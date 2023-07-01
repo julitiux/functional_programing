b628080562db1d8ba7d2221fb18ecbdfc65e6703
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-06-30 19:05:52 -0500

    implement the method computeStockWorth

--- src/main/java/com/functionalPrograming/chapter4/CalculateNAVImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/CalculateNAVImpl.java
@@ -1,4 +1,12 @@
 package com.functionalPrograming.chapter4;
 
-public class CalculateNAVImpl {
+import java.math.BigDecimal;
+
+public class CalculateNAVImpl implements CalculateNAV {
+
+  @Override
+  public BigDecimal computeStockWorth(String ticket, int shares) {
+    return princeFinder.apply(ticket).multiply(BigDecimal.valueOf(shares));
+  }
+
 }
