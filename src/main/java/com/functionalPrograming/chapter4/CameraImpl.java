3e67618f48d931cfac4cb2de5238a301a11d15bb
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-03 01:43:12 -0500

    implement method capture

--- src/main/java/com/functionalPrograming/chapter4/CameraImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/CameraImpl.java
@@ -1,4 +1,17 @@
 package com.functionalPrograming.chapter4;
 
+import java.awt.*;
+import java.util.function.Function;
+
 public class CameraImpl implements Camera{
+
+  Function<Color, Color> filter;
+
+  @Override
+  public Color capture(Color inputColor) {
+    Color processColor = filter.apply(inputColor);
+    //MORE PROCESSING OF COLOR...
+    return processColor;
+  }
+
 }
