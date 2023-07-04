c0d55d6d9c5587aab1471faf5799be60d3100cd7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-03 22:19:07 -0600

    declare method setFilters an void return

--- src/main/java/com/functionalPrograming/chapter4/Camera.java
+++ src/main/java/com/functionalPrograming/chapter4/Camera.java
@@ -1,7 +1,10 @@
 package com.functionalPrograming.chapter4;
 
 import java.awt.*;
+import java.util.function.Function;
 
 public interface Camera {
   Color capture(Color inputColor);
+
+  void setFilters(final Function<Color, Color>... filters);
 }
