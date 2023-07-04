21350a66dd11b322a1be6d5e48c17be17e305975
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 17:53:11 -0600

    implement a method handleException

--- src/main/java/com/functionalPrograming/chapter4/HandleExceptionImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/HandleExceptionImpl.java
@@ -1,4 +1,15 @@
 package com.functionalPrograming.chapter4;
 
+import java.io.File;
+import java.util.stream.Stream;
+
 public class HandleExceptionImpl implements HandleException{
+
+  @Override
+  public void handleException() {
+    Stream.of("/usr", "/tmp")
+      .map(path -> new File(path).getCanonicalPath())
+      .forEach(System.out::println);
+  }
+
 }
