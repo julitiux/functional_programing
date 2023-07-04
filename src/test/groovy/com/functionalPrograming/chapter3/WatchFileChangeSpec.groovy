37daceaef3e73164d49f8da94bb34b2d22376da9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-03 22:48:11 -0600

    add ignore test

--- src/test/groovy/com/functionalPrograming/chapter3/WatchFileChangeSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter3/WatchFileChangeSpec.groovy
@@ -1,9 +1,11 @@
 package com.functionalPrograming.chapter3
 
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class WatchFileChangeSpec extends Specification {
 
+  @Ignore
   def "watch file change"() {
     setup:
     WatchFileChange service = new WatchFileChangeImpl()
