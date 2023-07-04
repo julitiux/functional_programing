0f6df6949e827f1e4d0a9d8dbe84781f1c6687ec
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 17:55:39 -0600

    we could either handle the exception right there within the lambda expression

--- src/main/java/com/functionalPrograming/chapter4/HandleExceptionImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/HandleExceptionImpl.java
@@ -1,6 +1,7 @@
 package com.functionalPrograming.chapter4;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class HandleExceptionImpl implements HandleException{
@@ -8,7 +9,13 @@ public class HandleExceptionImpl implements HandleException{
   @Override
   public void handleException() {
     Stream.of("/usr", "/tmp")
-      .map(path -> new File(path).getCanonicalPath())
+      .map(path -> {
+        try {
+          return new File(path).getCanonicalPath();
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      })
       .forEach(System.out::println);
   }
 
