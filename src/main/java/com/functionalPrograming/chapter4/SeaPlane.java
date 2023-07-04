7ec47de21491b4a77164160f8b599b0793b4bed4
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 00:18:41 -0600

    implement method cruise; this class extend from Vehicle and implementes from FastFly and Sail

---
+++ src/main/java/com/functionalPrograming/chapter4/SeaPlane.java
@@ -0,0 +1,16 @@
+package com.functionalPrograming.chapter4;
+
+public class SeaPlane extends Vehicle implements FastFly, Sail {
+
+  private int altitude;
+
+  @Override
+  public void cruise() {
+    System.out.println("SeaPlane::cruise currently cruise like: ");
+    if (altitude > 0)
+      FastFly.super.cruise();
+    else
+      Sail.super.cruise();
+  }
+
+}
