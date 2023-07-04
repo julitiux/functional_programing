a45a3d0dffb8cc9b75df3c9f8f4f9ad653c8b6ee
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-03 22:49:33 -0600

    implement method print captured

--- src/test/groovy/com/functionalPrograming/chapter4/CameraSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter4/CameraSpec.groovy
@@ -2,5 +2,16 @@ package com.functionalPrograming.chapter4
 
 import spock.lang.Specification
 
-class CameraSpec extends Specification{
+import java.awt.*
+import java.util.function.Consumer
+
+class CameraSpec extends Specification {
+
+  def "print captured"() {
+    given:
+    Camera camera = new CameraImpl()
+    expect:
+    Consumer<String> printCaptured = (filterInfo) ->
+      println(String.format("with %s: %s", filterInfo, camera.capture(new Color(200, 100, 200))));
+  }
 }
