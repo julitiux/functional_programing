88f53743b923471b6a210eaec1d7d17ea7e38c2f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 22:02:38 -0600

    do an implementation with finaliza,writeStudd and FileWriterExampleImpl like constructor

--- src/main/java/com/functionalPrograming/chapter4/FileWriterExampleImpl.java
+++ src/main/java/com/functionalPrograming/chapter4/FileWriterExampleImpl.java
@@ -1,4 +1,22 @@
 package com.functionalPrograming.chapter4;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriterExampleImpl implements FileWriterExample {
+
+  private final FileWriter writer;
+
+  public FileWriterExampleImpl(final String fileName) throws IOException {
+    this.writer = new FileWriter(fileName);
+  }
+
+  public void writeStuff(final String message) throws IOException {
+    writer.write(message);
+  }
+
+  public void finaliza() throws IOException {
+    writer.close();
+  }
+
 }
