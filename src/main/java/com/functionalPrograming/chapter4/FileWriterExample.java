45375dd6305256fc728d753a5326a052b02002d7
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 22:01:48 -0600

    declare two method writeStuff and finaliza, every method has a thrwos by IOException

--- src/main/java/com/functionalPrograming/chapter4/FileWriterExample.java
+++ src/main/java/com/functionalPrograming/chapter4/FileWriterExample.java
@@ -1,4 +1,10 @@
 package com.functionalPrograming.chapter4;
 
+import java.io.IOException;
+
 public interface FileWriterExample {
+
+  void writeStuff(final String message) throws IOException;
+  void finaliza() throws IOException;
+
 }
