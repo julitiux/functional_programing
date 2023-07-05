9ffc3878682cd559e7ca5fd9d29e506354b71754
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-04 22:08:03 -0600

    implement unit test write stuff, calling the method service.writeStuff

--- src/test/groovy/com/functionalPrograming/chapter5/FileWriterExampleSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter5/FileWriterExampleSpec.groovy
@@ -2,7 +2,13 @@ package com.functionalPrograming.chapter5
 
 import spock.lang.Specification
 
-class FileWriterExampleSpec extends Specification{
+class FileWriterExampleSpec extends Specification {
 
-  
+  def "write stuff"() {
+    given:
+    FileWriterExample service = new FileWriterExampleImpl("peekaboo.txt")
+    expect:
+    service.writeStuff("peek-a-boo")
+  }
+
 }
