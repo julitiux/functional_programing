1468202c6f66e1a7cccdd0d96ad2feb7a6aa0b2a
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-05 23:32:08 -0600

    implement class HeavyImpl, implement a empty contructor and toString() method

---
+++ src/main/java/com/functionalPrograming/chapter6/HeavyImpl.java
@@ -0,0 +1,13 @@
+package com.functionalPrograming.chapter6;
+
+public class HeavyImpl {
+
+  public HeavyImpl() {
+    System.out.println("Heavy created");
+  }
+
+  @Override
+  public String toString() {
+    return "quite heavys";
+  }
+}
