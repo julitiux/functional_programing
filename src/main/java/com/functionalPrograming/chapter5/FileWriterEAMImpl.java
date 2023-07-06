5fdd462074025888931f3e6c8f1818b6380cfbc5
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-05 21:22:58 -0600

    implement method use

--- src/main/java/com/functionalPrograming/chapter5/FileWriterEAMImpl.java
+++ src/main/java/com/functionalPrograming/chapter5/FileWriterEAMImpl.java
@@ -1,4 +1,34 @@
 package com.functionalPrograming.chapter5;
 
-public class FileWriterEAMImpl implements FileWriterEAM{
+import com.functionalPrograming.chapter4.UseInstance;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEAMImpl implements FileWriterEAM {
+
+  private final FileWriter writer;
+
+  private FileWriterEAMImpl(final String fileName) throws IOException {
+    writer = new FileWriter(fileName);
+  }
+
+  private void close() throws IOException {
+    System.out.println("close called automatically... ");
+    writer.close();
+  }
+
+  public void writeStuff(final String message) throws IOException {
+    writer.write(message);
+  }
+
+  public static void use(final String fileName, final UseInstance<FileWriterEAM, IOException> block) throws IOException{
+    final FileWriterEAMImpl writerEAM = new FileWriterEAMImpl(fileName);
+    try {
+      block.accept(writerEAM);
+    }finally {
+      writerEAM.close();
+    }
+  }
+
 }
