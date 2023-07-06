afe7c932299db598f906910bbf68fb8bb5ba66d0
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-05 21:22:41 -0600

    declare method writeStuff

--- src/main/java/com/functionalPrograming/chapter5/FileWriterEAM.java
+++ src/main/java/com/functionalPrograming/chapter5/FileWriterEAM.java
@@ -1,4 +1,9 @@
 package com.functionalPrograming.chapter5;
 
+import java.io.IOException;
+
 public interface FileWriterEAM {
+
+  public void writeStuff(final String message) throws IOException;
+  
 }
