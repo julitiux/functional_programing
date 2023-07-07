b8bf11aa0fe7de3b18cea53c592ad2ee7c7f9f64
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 19:02:22 -0600

    implement method toUpper(String name)

--- src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
+++ src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
@@ -6,4 +6,10 @@ public class LazyStreams {
     System.out.println("getting length for " + name);
     return name.length();
   }
+
+  private static String toUpper(final String name) {
+    System.out.println("converting to uppercase: " + name);
+    return name.toUpperCase();
+  }
+
 }
