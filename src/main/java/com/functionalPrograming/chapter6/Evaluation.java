3b96d32e8b711847b69db926461d39a02e375634
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 18:45:04 -0600

    implement method eagerEvaluator(boolean input1, boolean input2)

--- src/main/java/com/functionalPrograming/chapter6/Evaluation.java
+++ src/main/java/com/functionalPrograming/chapter6/Evaluation.java
@@ -4,8 +4,18 @@ public class Evaluation {
 
   public static boolean evaluate(final int value) {
     System.out.println("evaluation... " + value);
-    simulateTimeConsumingOp(2000);
+    //simulateTimeConsumingOp(2000);
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
     return value > 100;
   }
-  
+
+  public static void eagerEvaluator(final boolean input1, final boolean input2) {
+    System.out.println("eagerEvaluaor called... ");
+    System.out.println("accept?: " + (input1 && input2));
+  }
+
 }
