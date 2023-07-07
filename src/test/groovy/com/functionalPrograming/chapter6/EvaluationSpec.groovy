a1a120ad466383fe399e2dca1243b8c3bbb8bcf6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 18:56:59 -0600

    implement unit test lazy Evaluator

--- src/test/groovy/com/functionalPrograming/chapter6/EvaluationSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter6/EvaluationSpec.groovy
@@ -2,13 +2,17 @@ package com.functionalPrograming.chapter6
 
 import spock.lang.Specification
 
-import static com.functionalPrograming.chapter6.Evaluation.eagerEvaluator
-import static com.functionalPrograming.chapter6.Evaluation.evaluate
+import static com.functionalPrograming.chapter6.Evaluation.*
 
-class EvaluationSpec extends Specification{
+class EvaluationSpec extends Specification {
 
-  def"eager Evaluator"(){
+  def "eager Evaluator"() {
     expect:
     eagerEvaluator(evaluate(1), evaluate(2))
+  }
+
+  def "lazy Evaluator"() {
+    expect:
+    lazyEvaluator(() -> evaluate(1), () -> evaluate(2))
   }
 }
