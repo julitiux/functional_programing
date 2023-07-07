9831d3fa983cb2f4009498c1da668b0498e43cb9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-06 19:07:11 -0600

    consuming the list using method statics like length and toUpper into the Stream()

--- src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
+++ src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
@@ -1,5 +1,8 @@
 package com.functionalPrograming.chapter6;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LazyStreams {
 
   private static int length(final String name) {
@@ -11,5 +14,14 @@ public class LazyStreams {
     System.out.println("converting to uppercase: " + name);
     return name.toUpperCase();
   }
+
+  List<String> names = Arrays.asList("Brad", "Kate", "Kim", "Jack", "Joe", "Mike", "Susan", "George", "Robert", "Julia", "Parker", "Benson");
+
+  final String firstNameWith3Letters =
+    names.stream()
+      .filter(name -> length(name) == 3)
+      .map(name -> toUpper(name))
+      .findFirst()
+      .get();
 
 }
