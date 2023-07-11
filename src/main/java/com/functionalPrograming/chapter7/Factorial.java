d5a61077b7b518eef695a977b346e9fa23934b01
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-11 10:05:49 -0600

    implement method factorialTailRec and return a TailCall<Integer> and recive two params factorial and number

--- src/main/java/com/functionalPrograming/chapter7/Factorial.java
+++ src/main/java/com/functionalPrograming/chapter7/Factorial.java
@@ -1,5 +1,9 @@
 package com.functionalPrograming.chapter7;
 
+import static fpij.TailCalls.call;
+import static fpij.TailCalls.done;
+
+
 public class Factorial {
 
   public static int factorialRec(final int number) {
@@ -7,6 +11,14 @@ public class Factorial {
       return number;
     else
       return number * factorialRec(number - 1);
+  }
+
+
+  public static TailCall<Integer> factorialTailRec(final int factorial, final int number) {
+    if (number == 1)
+      return done(factorial);
+    else
+      return call(() -> factorialTailRec(factorial * number, number - 1));
   }
 
 }
