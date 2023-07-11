b977cb28479c7380e4c786622adfe8b787c055d8
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-11 09:02:16 -0600

    implement method factorialRec()

---
+++ src/main/java/com/functionalPrograming/chapter7/Factorial.java
@@ -0,0 +1,12 @@
+package com.functionalPrograming.chapter7;
+
+public class Factorial {
+
+  public static int factorialRec(final int number) {
+    if (number == 1)
+      return number;
+    else
+      return number + factorialRec(number - 1);
+  }
+
+}
