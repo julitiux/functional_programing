e833da2f63685146f6755f6b1467cdbf72e9a6a5
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-11 11:48:13 -0600

    where we used int in the early version, we used BigInteger in this version

--- src/main/java/com/functionalPrograming/chapter7/BigFactorial.java
+++ src/main/java/com/functionalPrograming/chapter7/BigFactorial.java
@@ -2,6 +2,9 @@ package com.functionalPrograming.chapter7;
 
 import java.math.BigInteger;
 
+import static com.functionalPrograming.chapter7.TailCalls.call;
+import static com.functionalPrograming.chapter7.TailCalls.done;
+
 public class BigFactorial {
 
   public static BigInteger decrement(final BigInteger number) {
@@ -15,5 +18,12 @@ public class BigFactorial {
   final static BigInteger ONE = BigInteger.ONE;
   final static BigInteger FIVE = new BigInteger("5");
   final static BigInteger TWENTK = new BigInteger("20000");
+
+  public static TailCall<BigInteger> factorialTailRec(final BigInteger factorial, final BigInteger number) {
+    if (number.equals(BigInteger.ONE))
+      return done(factorial);
+    else
+      return call(() -> factorialTailRec(multiply(factorial, number), decrement(number)));
+  }
 
 }
