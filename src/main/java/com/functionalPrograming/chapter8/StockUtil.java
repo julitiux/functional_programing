66a4a20d2acb7c3680799de8edbb61ea9e088300
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-12 22:47:38 -0600

    takes a price value and returns a Predicate that can be evaluated later to check if a given instance of StockInfo is less than the price value cached in the lambda expression

--- src/main/java/com/functionalPrograming/chapter8/StockUtil.java
+++ src/main/java/com/functionalPrograming/chapter8/StockUtil.java
@@ -1,9 +1,16 @@
 package com.functionalPrograming.chapter8;
 
+import java.math.BigDecimal;
+import java.util.function.Predicate;
+
 public class StockUtil {
 
-  public static StockInfo getPrice(final String ticker){
+  public static StockInfo getPrice(final String ticker) {
     return new StockInfo(ticker, YahooFinance.getPrice(ticker));
+  }
+
+  public static Predicate<StockInfo> isPriceLessThan(final int price) {
+    return stockInfo -> stockInfo.price.compareTo(BigDecimal.valueOf(price)) < 0;
   }
 
 }
