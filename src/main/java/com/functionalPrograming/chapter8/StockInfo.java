b1f96981de0f68cc6b3f97e2a46771c417985d7f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-12 22:39:45 -0600

    implement StockInfo class

---
+++ src/main/java/com/functionalPrograming/chapter8/StockInfo.java
@@ -0,0 +1,19 @@
+package com.functionalPrograming.chapter8;
+
+import java.math.BigDecimal;
+
+public class StockInfo {
+
+  public final String ticker;
+  public final BigDecimal price;
+
+  public StockInfo(final String symbol, final BigDecimal thePrice) {
+    ticker = symbol;
+    price = thePrice;
+  }
+
+  public String toString() {
+    return String.format("ticker: %s price %g ", ticker, price);
+  }
+  
+}
