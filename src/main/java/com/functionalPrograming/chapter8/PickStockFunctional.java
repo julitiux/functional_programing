cee2f6d0a1ab4528a97456aeaa0cd62e22c2dbfa
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-12 23:18:02 -0600

    In the method findHighPriced(), we employ method chaining and transform objects. We start with a Stream of symbols and flow into streams of stocks. We first map the symbols into stocks filled with prices. Following the map operation, we trim down the list and reduce it to a single value. The reduce() function gives us some extra control, but if we simply want to pick the highest value, we could instead use the max() method on the Stream

---
+++ src/main/java/com/functionalPrograming/chapter8/PickStockFunctional.java
@@ -0,0 +1,17 @@
+package com.functionalPrograming.chapter8;
+
+import java.util.stream.Stream;
+
+public class PickStockFunctional {
+
+  public static void findHighPriced(final Stream<String> symbols) {
+    final StockInfo highPriced =
+      symbols.map(StockUtil::getPrice)
+        .filter(StockUtil.isPriceLessThan(500))
+        .reduce(StockUtil::pickHigh)
+        .get();
+
+    System.out.println("High priced under $500 is " + highPriced);
+  }
+
+}
