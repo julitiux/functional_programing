c6ac39eef0fe0824bf64406b39f66013229209c2
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-13 13:05:30 -0600

    delete import unnecesary

--- src/test/groovy/com/functionalPrograming/chapter8/PickStockFunctionalSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter8/PickStockFunctionalSpec.groovy
@@ -3,8 +3,6 @@ package com.functionalPrograming.chapter8
 import spock.lang.Shared
 import spock.lang.Specification
 
-import java.util.stream.Stream
-
 class PickStockFunctionalSpec extends Specification {
 
   @Shared
