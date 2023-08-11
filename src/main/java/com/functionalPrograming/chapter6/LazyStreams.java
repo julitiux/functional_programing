9ce2482a6bd52380dd06b43d9d32aa632febab0f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-10 23:10:55 -0600

    format code

--- src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
+++ src/main/java/com/functionalPrograming/chapter6/LazyStreams.java
@@ -30,14 +30,11 @@ public class LazyStreams {
       names.stream()
         .filter(name -> length(name) == 3)
         .map(name -> toUpper(name));
-
     System.out.println("Stream created, filtered, mapped...");
     System.out.println("ready to call findFirst...");
-
     final String firstNameWith3Letters =
       namesWith3Letters.findFirst()
         .get();
-
     System.out.println(firstNameWith3Letters);
   }
 
