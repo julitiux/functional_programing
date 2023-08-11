de784aba6d7bf9169463b0964f2d3031b181c113
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-10 23:11:23 -0600

    implement class LazyStreamsGRoovy just for test the method testLazyMethods() on groovy

--- src/test/groovy/com/functionalPrograming/chapter6/LazyStreamsSpec.groovy
+++ src/test/groovy/com/functionalPrograming/chapter6/LazyStreamsSpec.groovy
@@ -1,16 +1,45 @@
 package com.functionalPrograming.chapter6
 
-import spock.lang.Shared
+
 import spock.lang.Specification
 
 class LazyStreamsSpec extends Specification {
 
-  @Shared
-    service = new LazyStreams()
-
   def "Test Lazy Method"() {
+    given:
+    LazyStreams service = new LazyStreams()
     expect:
     service.testLazyMethod()
+  }
+
+  def "other test on Groovy"() {
+    given:
+    def instance = new LazyStreamsGRoovy()
+    expect:
+    instance.testLazyMethods()
+  }
+
+}
+
+
+class LazyStreamsGRoovy {
+
+  int length(String name) {
+    println "getting length for " + name
+    name.length()
+  }
+
+  String toUpper(String name) {
+    println "converting to uppercase: " + name
+    name.toUpperCase()
+  }
+
+  void testLazyMethods() {
+    List names = ["Brad", "Kate", "Kim", "Jack", "Joe", "Mike", "Susan", "George", "Robert", "Julia", "Parker", "Benson"]
+    def namesWith3Letters = names.find { name -> length(name) == 3 }
+    println "zzz Stream created, filtered, mapped..."
+    println "zzz ready to call findFirst..."
+    println namesWith3Letters
   }
 
 }
